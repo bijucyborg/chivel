d0ac1397d32ae48777a7cabb796d67fc980a5bae
Biju Krishnan <biju.krishnan@gmail.com>
2024-08-17 11:20:17 +0200

    commented @grikomsn/cal-sans in package.json

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import '@grikomsn/cal-sans';
+//import '@grikomsn/cal-sans';
 import { UserContextProvider } from '@/utils/contexts/useUser';
 import { SWRConfig } from 'swr';
 import { Toaster } from 'react-hot-toast';
